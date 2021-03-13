007e48d82c57bd0793caa5ac614ecc6ab0464075
semanurtopal <77543320+semanurtopal@users.noreply.github.com>
2021-03-13 21:55:10 +0300

    13martcommit1

--- kamp-frontend/northwind/src/app/services/product.service.ts
+++ kamp-frontend/northwind/src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { ProductResponseModel } from '../models/productResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class ProductService {
   
   constructor(private httpClient:HttpClient) { }
 
-  getProducts():Observable<ProductResponseModel>{
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  getProducts():Observable<ListResponseModel<Product>>{
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
   }
 }
