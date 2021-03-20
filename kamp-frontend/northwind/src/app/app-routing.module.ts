7fbc9792bcd44525b1d8437a1ff38c5347cc61a7
semanurtopal <77543320+semanurtopal@users.noreply.github.com>
2021-03-21 00:03:34 +0300

    20 mart ders sonu

--- kamp-frontend/northwind/src/app/app-routing.module.ts
+++ kamp-frontend/northwind/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { ProductComponent } from './components/product/product.component';
 const routes: Routes = [
   {path:"",pathMatch:"full", component:ProductComponent},
   {path:"products", component:ProductComponent},
-  {path:"products/category/:categoryId", component:ProductComponent}
+  {path:"products/category/:categoryId", component:ProductComponent},
+  {path:"products/add",component:ProductComponent}
 ];
 
 @NgModule({
